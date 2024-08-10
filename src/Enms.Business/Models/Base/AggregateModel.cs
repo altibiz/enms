a5a5cadb87b0bd7daf0920f9d0fbc70a529ce5b5
Hrvoje Jurić <hrvoje@altibiz.com>
2024-08-10 18:32:34 +0200

    WIP

--- src/Enms.Business/Models/Base/AggregateModel.cs
+++ src/Enms.Business/Models/Base/AggregateModel.cs
@@ -1,6 +1,8 @@
 using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using Enms.Business.Models.Abstractions;
 using Enms.Business.Models.Enums;
+using Enms.Business.Time;
 
 namespace Enms.Business.Models.Base;
 
@@ -12,12 +14,42 @@ public abstract class AggregateModel : IAggregate
   [Required]
   public required string LineId { get; set; }
 
-  [Required]
-  public required DateTimeOffset Timestamp { get; init; } =
-    DateTimeOffset.UtcNow;
+  private IntervalModel _interval = IntervalModel.QuarterHour;
+
+  // NOTE: just so it doesn't break if interval is set before timestamp
+  private DateTimeOffset _timestamp =
+    DateTimeOffset.Parse("2000-01-01T00:00:00Z", CultureInfo.InvariantCulture);
 
   [Required]
-  public required IntervalModel Interval { get; init; }
+  public required DateTimeOffset Timestamp
+  {
+    get { return _timestamp.ToUniversalTime(); }
+    set
+    {
+      _ = Interval switch
+      {
+#pragma warning disable S1121
+        IntervalModel.QuarterHour => _timestamp = value.GetStartOfQuarterHour(),
+        IntervalModel.Day => _timestamp = value.GetStartOfDay(),
+        IntervalModel.Month => _timestamp = value.GetStartOfMonth(),
+#pragma warning restore S1121
+        _ => throw new InvalidOperationException(
+          $"Unsupported interval {Interval}"
+        )
+      };
+    }
+  }
+
+  [Required]
+  public required IntervalModel Interval
+  {
+    get { return _interval; }
+    set
+    {
+      _interval = value;
+      Timestamp = _timestamp;
+    }
+  }
 
   [Required]
   public required long Count { get; init; } = 0;
