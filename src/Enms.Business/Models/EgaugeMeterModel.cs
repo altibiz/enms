dd588f7cfde7e7d27a89ce96f187d2ed2c3d456d
Hrvoje Jurić <hrvoje@altibiz.com>
2024-08-10 22:52:04 +0200

    WIP

--- src/Enms.Business/Models/EgaugeMeterModel.cs
+++ src/Enms.Business/Models/EgaugeMeterModel.cs
@@ -2,6 +2,8 @@ using Enms.Business.Models.Base;
 
 namespace Enms.Business.Models;
 
+#pragma warning disable S2094 // Classes should not be empty
 public class EgaugeMeterModel : MeterModel
+#pragma warning restore S2094 // Classes should not be empty
 {
 }
