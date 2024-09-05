6e6702d2c09c2e9d1a98a225700868353ba8f820
Hrvoje Jurić <hrvoje@altibiz.com>
2024-09-05 05:03:58 +0200

    WIP

--- src/Enms.Data/Migrations/20240905030123_Init4.Designer.cs
+++ src/Enms.Data/Migrations/20240905030123_Init4.Designer.cs
@@ -5,6 +5,7 @@ using System.Text.Json;
 using Enms.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -13,9 +14,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Enms.Data.Migrations
 {
     [DbContext(typeof(DataDbContext))]
-    partial class DataDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240905030123_Init4")]
+    partial class Init4
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
