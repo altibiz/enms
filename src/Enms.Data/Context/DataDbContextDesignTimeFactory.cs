bf0149393ff5c8db226eb78e1961eb35af6d3ab4
Hrvoje Jurić <hrvoje@altibiz.com>
2024-09-05 06:33:15 +0200

    WIP

--- src/Enms.Data/Context/DataDbContextDesignTimeFactory.cs
+++ src/Enms.Data/Context/DataDbContextDesignTimeFactory.cs
@@ -1,6 +1,7 @@
 using Enms.Data.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Npgsql;
 
 namespace Enms.Data.Context;
 
@@ -10,15 +11,23 @@ public class
   public DataDbContext CreateDbContext(string[] args)
   {
     var optionsBuilder = new DbContextOptionsBuilder<DataDbContext>();
+
+    var dataSourceBuilder =
+    new NpgsqlDataSourceBuilder(
+      "Server=localhost;Port=5432;User Id=enms;Password=enms;Database=enms");
+    dataSourceBuilder.ApplyConfigurationsFromAssembly(
+      typeof(DataDbContext).Assembly);
+
     optionsBuilder.UseTimescale(
-      "Server=localhost;Port=5432;User Id=enms;Password=enms;Database=enms",
-      x =>
-      {
-        x.MigrationsAssembly(
-          typeof(DataDbContext).Assembly.GetName().Name);
-        x.MigrationsHistoryTable(
-          $"__Enms{nameof(DataDbContext)}");
-      });
+     dataSourceBuilder.Build(),
+     x =>
+     {
+       x.MigrationsAssembly(
+         typeof(DataDbContext).Assembly.GetName().Name);
+       x.MigrationsHistoryTable(
+         $"__Enms{nameof(DataDbContext)}");
+     });
+
     return new DataDbContext(optionsBuilder.Options);
   }
 }
