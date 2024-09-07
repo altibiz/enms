7d668157e4d6c9d72c9c560bd0edbaeda1f776cf
Hrvoje Jurić <hrvoje@altibiz.com>
2024-09-07 14:01:14 +0200

    WIP

--- src/Enms.Data/Extensions/MigrationBuilderExtensions.cs
+++ src/Enms.Data/Extensions/MigrationBuilderExtensions.cs
@@ -1,334 +1,212 @@
 using System.Text;
 using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace Enms.Data.Extensions;
-
-public static class MigrationBuilderExtensions
+namespace Enms.Data.Extensions
 {
-  public static void ConvertIntToEnum<TEnum>(
-    this MigrationBuilder migrationBuilder,
-    string tableName,
-    string columnName)
-    where TEnum : Enum
+  public static class MigrationBuilderExtensions
   {
-    var enumValues = Enum.GetValues(typeof(TEnum)).Cast<TEnum>();
-    var enumTypeName = typeof(TEnum).Name.ToSnakeCase();
-
-    var sql = new StringBuilder();
-
-    sql.AppendLine(
-      $@"
-        ALTER TABLE {
-          tableName
-        }
-        ADD COLUMN temp_{
-          columnName
-        } {
-          enumTypeName
-        };
-    ");
-
-    sql.AppendLine(
-      $@"
-        UPDATE {
-          tableName
-        }
-        SET temp_{
-          columnName
-        } = CASE");
-
-    foreach (var enumValue in enumValues)
+    public static void ConvertIntToEnum(
+      this MigrationBuilder migrationBuilder,
+      string tableName,
+      string columnName,
+      Dictionary<int, string> intToEnumMapping,
+      string enumTypeName
+    )
     {
-      var enumIndex = Convert.ToInt32(enumValue);
-      var enumName = enumValue.ToString().ToSnakeCase();
+      var sql = new StringBuilder();
+
       sql.AppendLine(
         $@"
-            WHEN {
-              columnName
-            } = {
-              enumIndex
-            } THEN '{
-              enumName
-            }'::{
-              enumTypeName
-            }");
-    }
+                ALTER TABLE {tableName}
+                ADD COLUMN temp_{columnName} {enumTypeName};
+            ");
 
-    sql.AppendLine(
-      @"
-        END;
-    ");
-
-    sql.AppendLine(
-      $@"
-        ALTER TABLE {
-          tableName
-        }
-        DROP COLUMN {
-          columnName
-        };
-    ");
-
-    sql.AppendLine(
-      $@"
-        ALTER TABLE {
-          tableName
-        }
-        RENAME COLUMN temp_{
-          columnName
-        } TO {
-          columnName
-        };
-    ");
-
-    migrationBuilder.Sql(sql.ToString());
-  }
-
-  public static void ConvertEnumToInt<TEnum>(
-    this MigrationBuilder migrationBuilder,
-    string tableName,
-    string columnName)
-    where TEnum : Enum
-  {
-    var enumValues = Enum.GetValues(typeof(TEnum)).Cast<TEnum>();
-
-    var sql = new StringBuilder();
-
-    sql.AppendLine(
-      $@"
-        ALTER TABLE {
-          tableName
-        }
-        ADD COLUMN temp_{
-          columnName
-        } integer;
-    ");
-
-    sql.AppendLine(
-      $@"
-        UPDATE {
-          tableName
-        }
-        SET temp_{
-          columnName
-        } = CASE");
-
-    foreach (var enumValue in enumValues)
-    {
-      var enumIndex = Convert.ToInt32(enumValue);
-      var enumName = enumValue.ToString().ToSnakeCase();
       sql.AppendLine(
         $@"
-            WHEN {
-              columnName
-            } = '{
-              enumName
-            }' THEN {
-              enumIndex
-            }");
-    }
+                UPDATE {tableName}
+                SET temp_{columnName} = CASE");
 
-    sql.AppendLine(
-      @"
-        END;
-    ");
+      foreach (var kvp in intToEnumMapping)
+      {
+        var enumIndex = kvp.Key;
+        var enumName = kvp.Value;
+        sql.AppendLine(
+          $@"
+                    WHEN {columnName} = {enumIndex} THEN '{enumName}'::{enumTypeName}");
+      }
 
-    sql.AppendLine(
-      $@"
-        ALTER TABLE {
-          tableName
-        }
-        DROP COLUMN {
-          columnName
-        };
-    ");
+      sql.AppendLine(
+        @"
+                END;
+            ");
 
-    sql.AppendLine(
-      $@"
-        ALTER TABLE {
-          tableName
-        }
-        RENAME COLUMN temp_{
-          columnName
-        } TO {
-          columnName
-        };
-    ");
-
-    migrationBuilder.Sql(sql.ToString());
-  }
-
-  public static void ConvertIntArrayToEnumArray<TEnum>(
-    this MigrationBuilder migrationBuilder,
-    string tableName,
-    string columnName
-  )
-    where TEnum : Enum
-  {
-    var enumValues = Enum.GetValues(typeof(TEnum)).Cast<TEnum>();
-    var enumTypeName = typeof(TEnum).Name.ToSnakeCase();
-
-    var sql = new StringBuilder();
-    sql.AppendLine(
-      $@"
-        ALTER TABLE {
-          tableName
-        }
-        ADD COLUMN temp_{
-          columnName
-        } {
-          enumTypeName
-        }[];
-    ");
-
-    sql.AppendLine(
-      $@"
-        UPDATE {
-          tableName
-        }
-        SET temp_{
-          columnName
-        } = ARRAY(
-            SELECT CASE");
-
-    foreach (var enumValue in enumValues)
-    {
-      var enumIndex = Convert.ToInt32(enumValue);
-      var enumName = enumValue.ToString().ToSnakeCase();
       sql.AppendLine(
         $@"
-        WHEN {
-          columnName
-        }_values = {
-          enumIndex
-        } THEN '{
-          enumName
-        }'::{
-          enumTypeName
-        }");
-    }
+                ALTER TABLE {tableName}
+                DROP COLUMN {columnName};
+            ");
 
-    sql.AppendLine(
-      $@"
-        END
-
-        FROM unnest({
-          columnName
-        }) AS {
-          columnName
-        }_values
-      );
-    ");
-
-    sql.AppendLine(
-      $@"
-        ALTER TABLE {
-          tableName
-        }
-        DROP COLUMN {
-          columnName
-        };
-    ");
-
-    sql.AppendLine(
-      $@"
-        ALTER TABLE {
-          tableName
-        }
-        RENAME COLUMN temp_{
-          columnName
-        } TO {
-          columnName
-        };
-    ");
-
-    migrationBuilder.Sql(sql.ToString());
-  }
-
-  public static void ConvertEnumArrayToIntArray<TEnum>(
-    this MigrationBuilder migrationBuilder,
-    string tableName,
-    string columnName)
-    where TEnum : Enum
-  {
-    var enumValues = Enum.GetValues(typeof(TEnum)).Cast<TEnum>();
-
-    var sql = new StringBuilder();
-    sql.AppendLine(
-      $@"
-        ALTER TABLE {
-          tableName
-        }
-        ADD COLUMN temp_{
-          columnName
-        } integer[];
-    ");
-
-    sql.AppendLine(
-      $@"
-        UPDATE {
-          tableName
-        }
-        SET temp_{
-          columnName
-        } = ARRAY(
-            SELECT CASE");
-
-    foreach (var enumValue in enumValues)
-    {
-      var enumIndex = Convert.ToInt32(enumValue);
-      var enumName = enumValue.ToString().ToSnakeCase();
       sql.AppendLine(
         $@"
-        WHEN {
-          columnName
-        }_values = '{
-          enumName
-        }' THEN {
-          enumIndex
-        }");
+                ALTER TABLE {tableName}
+                RENAME COLUMN temp_{columnName} TO {columnName};
+            ");
+
+      migrationBuilder.Sql(sql.ToString());
     }
 
-    sql.AppendLine(
-      $@"
-        END
-        FROM unnest({
-          columnName
-        }) AS {
-          columnName
-        }_values
-      );
-    ");
+    public static void ConvertEnumToInt(
+      this MigrationBuilder migrationBuilder,
+      string tableName,
+      string columnName,
+      Dictionary<string, int> enumToIntMapping
+    )
+    {
+      var sql = new StringBuilder();
 
-    sql.AppendLine(
-      $@"
-        ALTER TABLE {
-          tableName
-        }
-        DROP COLUMN {
-          columnName
-        };
-    ");
+      sql.AppendLine(
+        $@"
+                ALTER TABLE {tableName}
+                ADD COLUMN temp_{columnName} integer;
+            ");
 
-    sql.AppendLine(
-      $@"
-        ALTER TABLE {
-          tableName
-        }
-        RENAME COLUMN temp_{
-          columnName
-        } TO {
-          columnName
-        };
-    ");
+      sql.AppendLine(
+        $@"
+                UPDATE {tableName}
+                SET temp_{columnName} = CASE");
 
-    migrationBuilder.Sql(sql.ToString());
-  }
+      foreach (var kvp in enumToIntMapping)
+      {
+        var enumName = kvp.Key;
+        var enumIndex = kvp.Value;
+        sql.AppendLine(
+          $@"
+                    WHEN {columnName} = '{enumName}' THEN {enumIndex}");
+      }
 
-  private static string ToSnakeCase(this string str)
-  {
-    return string.Concat(
-        str.Select((x, i) => i > 0 && char.IsUpper(x) ? "_" + x : x.ToString()))
-      .ToLower();
+      sql.AppendLine(
+        @"
+                END;
+            ");
+
+      sql.AppendLine(
+        $@"
+                ALTER TABLE {tableName}
+                DROP COLUMN {columnName};
+            ");
+
+      sql.AppendLine(
+        $@"
+                ALTER TABLE {tableName}
+                RENAME COLUMN temp_{columnName} TO {columnName};
+            ");
+
+      migrationBuilder.Sql(sql.ToString());
+    }
+
+    public static void ConvertIntArrayToEnumArray(
+      this MigrationBuilder migrationBuilder,
+      string tableName,
+      string columnName,
+      Dictionary<int, string> intToEnumMapping,
+      string enumTypeName
+    )
+    {
+      var sql = new StringBuilder();
+
+      sql.AppendLine(
+        $@"
+                ALTER TABLE {tableName}
+                ADD COLUMN temp_{columnName} {enumTypeName}[];
+            ");
+
+      sql.AppendLine(
+        $@"
+                UPDATE {tableName}
+                SET temp_{columnName} = ARRAY(
+                    SELECT CASE");
+
+      foreach (var kvp in intToEnumMapping)
+      {
+        var enumIndex = kvp.Key;
+        var enumName = kvp.Value;
+        sql.AppendLine(
+          $@"
+                    WHEN {columnName}_values = {enumIndex} THEN '{enumName}'::{enumTypeName}");
+      }
+
+      sql.AppendLine(
+        $@"
+                    END
+                FROM unnest({columnName}) AS {columnName}_values
+              );
+            ");
+
+      sql.AppendLine(
+        $@"
+                ALTER TABLE {tableName}
+                DROP COLUMN {columnName};
+            ");
+
+      sql.AppendLine(
+        $@"
+                ALTER TABLE {tableName}
+                RENAME COLUMN temp_{columnName} TO {columnName};
+            ");
+
+      migrationBuilder.Sql(sql.ToString());
+    }
+
+    public static void ConvertEnumArrayToIntArray(
+      this MigrationBuilder migrationBuilder,
+      string tableName,
+      string columnName,
+      Dictionary<string, int> enumToIntMapping
+    )
+    {
+      var sql = new StringBuilder();
+
+      sql.AppendLine(
+        $@"
+                ALTER TABLE {tableName}
+                ADD COLUMN temp_{columnName} integer[];
+            ");
+
+      sql.AppendLine(
+        $@"
+                UPDATE {tableName}
+                SET temp_{columnName} = ARRAY(
+                    SELECT CASE");
+
+      foreach (var kvp in enumToIntMapping)
+      {
+        var enumName = kvp.Key;
+        var enumIndex = kvp.Value;
+        sql.AppendLine(
+          $@"
+                    WHEN {columnName}_values = '{enumName}' THEN {enumIndex}");
+      }
+
+      sql.AppendLine(
+        $@"
+                    END
+                FROM unnest({columnName}) AS {columnName}_values
+              );
+            ");
+
+      sql.AppendLine(
+        $@"
+                ALTER TABLE {tableName}
+                DROP COLUMN {columnName};
+            ");
+
+      sql.AppendLine(
+        $@"
+                ALTER TABLE {tableName}
+                RENAME COLUMN temp_{columnName} TO {columnName};
+            ");
+
+      migrationBuilder.Sql(sql.ToString());
+    }
   }
 }
--- src/Enms.Data/Migrations/20240905044313_Init5.cs
+++ src/Enms.Data/Migrations/20240905044313_Init5.cs
@@ -3,6 +3,8 @@ using Enms.Data.Entities.Enums;
 using Enms.Data.Extensions;
 using Microsoft.EntityFrameworkCore.Migrations;
 
+#pragma warning disable S3887
+#pragma warning disable S2386
 #nullable disable
 
 namespace Enms.Data.Migrations
@@ -23,23 +25,7 @@ namespace Enms.Data.Migrations
                 table: "meters",
                 newName: "max_inactivity_period_duration");
 
-            migrationBuilder.ConvertIntArrayToEnumArray<TopicEntity>("representatives", "topics");
-
-            migrationBuilder.ConvertIntToEnum<RoleEntity>("representatives", "role");
-
-            migrationBuilder.ConvertIntArrayToEnumArray<TopicEntity>("notifications", "topics");
-
-            migrationBuilder.ConvertIntToEnum<DurationEntity>("meters", "max_inactivity_period_duration");
-
-            migrationBuilder.ConvertIntArrayToEnumArray<PhaseEntity>("lines", "phases");
-
-            migrationBuilder.ConvertIntToEnum<LevelEntity>("events", "level");
-
-            migrationBuilder.ConvertIntArrayToEnumArray<CategoryEntity>("events", "categories");
-
-            migrationBuilder.ConvertIntToEnum<AuditEntity>("events", "audit");
-
-            migrationBuilder.ConvertIntToEnum<IntervalEntity>("egauge_aggregates", "interval");
+            EnumMappings.ApplyUpMigrations(migrationBuilder);
 
             migrationBuilder.Sql(@"
                 ALTER TABLE egauge_aggregates
@@ -50,6 +36,8 @@ namespace Enms.Data.Migrations
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            EnumMappings.ApplyDownMigrations(migrationBuilder);
+
             migrationBuilder.RenameColumn(
                 name: "max_inactivity_period_multiplier",
                 table: "meters",
@@ -59,24 +47,258 @@ namespace Enms.Data.Migrations
                 name: "max_inactivity_period_duration",
                 table: "meters",
                 newName: "inactivity_duration_duration");
+        }
 
-            migrationBuilder.ConvertEnumArrayToIntArray<TopicEntity>("representatives", "topics");
+        public static class EnumMappings
+        {
+            public static readonly Dictionary<int, string> AuditEntityIntToEnum = new Dictionary<int, string>
+            {
+                { 0, "query" },
+                { 1, "creation" },
+                { 2, "modification" },
+                { 3, "deletion" }
+            };
 
-            migrationBuilder.ConvertEnumToInt<RoleEntity>("representatives", "role");
+            public static readonly Dictionary<string, int> AuditEntityEnumToInt = new Dictionary<string, int>
+            {
+                { "query", 0 },
+                { "creation", 1 },
+                { "modification", 2 },
+                { "deletion", 3 }
+            };
 
-            migrationBuilder.ConvertEnumArrayToIntArray<TopicEntity>("notifications", "topics");
+            public static readonly Dictionary<int, string> CategoryEntityIntToEnum = new Dictionary<int, string>
+            {
+                { 0, "all" },
+                { 1, "meter" },
+                { 2, "meter_push" }
+            };
 
-            migrationBuilder.ConvertEnumToInt<DurationEntity>("meters", "max_inactivity_period_duration");
+            public static readonly Dictionary<string, int> CategoryEntityEnumToInt = new Dictionary<string, int>
+            {
+                { "all", 0 },
+                { "meter", 1 },
+                { "meter_push", 2 }
+            };
 
-            migrationBuilder.ConvertEnumArrayToIntArray<PhaseEntity>("lines", "phases");
+            public static readonly Dictionary<int, string> DurationEntityIntToEnum = new Dictionary<int, string>
+            {
+                { 0, "second" },
+                { 1, "minute" },
+                { 2, "hour" },
+                { 3, "day" },
+                { 4, "week" },
+                { 5, "month" },
+                { 6, "year" }
+            };
 
-            migrationBuilder.ConvertEnumToInt<LevelEntity>("events", "level");
+            public static readonly Dictionary<string, int> DurationEntityEnumToInt = new Dictionary<string, int>
+            {
+                { "second", 0 },
+                { "minute", 1 },
+                { "hour", 2 },
+                { "day", 3 },
+                { "week", 4 },
+                { "month", 5 },
+                { "year", 6 }
+            };
 
-            migrationBuilder.ConvertEnumArrayToIntArray<CategoryEntity>("events", "categories");
+            public static readonly Dictionary<int, string> LevelEntityIntToEnum = new Dictionary<int, string>
+            {
+                { 0, "trace" },
+                { 1, "debug" },
+                { 2, "info" },
+                { 3, "warning" },
+                { 4, "error" },
+                { 5, "critical" }
+            };
 
-            migrationBuilder.ConvertEnumToInt<AuditEntity>("events", "audit");
+            public static readonly Dictionary<string, int> LevelEntityEnumToInt = new Dictionary<string, int>
+            {
+                { "trace", 0 },
+                { "debug", 1 },
+                { "info", 2 },
+                { "warning", 3 },
+                { "error", 4 },
+                { "critical", 5 }
+            };
 
-            migrationBuilder.ConvertEnumToInt<IntervalEntity>("egauge_aggregates", "interval");
+            public static readonly Dictionary<int, string> PhaseEntityIntToEnum = new Dictionary<int, string>
+            {
+                { 0, "l1" },
+                { 1, "l2" },
+                { 2, "l3" }
+            };
+
+            public static readonly Dictionary<string, int> PhaseEntityEnumToInt = new Dictionary<string, int>
+            {
+                { "l1", 0 },
+                { "l2", 1 },
+                { "l3", 2 }
+            };
+
+            public static readonly Dictionary<int, string> TopicEntityIntToEnum = new Dictionary<int, string>
+            {
+                { 0, "all" },
+                { 1, "meter" },
+                { 2, "meter_inactivity" }
+            };
+
+            public static readonly Dictionary<string, int> TopicEntityEnumToInt = new Dictionary<string, int>
+            {
+                { "all", 0 },
+                { "meter", 1 },
+                { "meter_inactivity", 2 }
+            };
+
+            public static readonly Dictionary<int, string> RoleEntityIntToEnum = new Dictionary<int, string>
+            {
+                { 0, "operator_representative" },
+                { 1, "user_representative" }
+            };
+
+            public static readonly Dictionary<string, int> RoleEntityEnumToInt = new Dictionary<string, int>
+            {
+                { "operator_representative", 0 },
+                { "user_representative", 1 }
+            };
+
+            public static readonly Dictionary<int, string> IntervalEntityIntToEnum = new Dictionary<int, string>
+            {
+                { 0, "quarter_hour" },
+                { 1, "day" },
+                { 2, "month" }
+            };
+
+            public static readonly Dictionary<string, int> IntervalEntityEnumToInt = new Dictionary<string, int>
+            {
+                { "quarter_hour", 0 },
+                { "day", 1 },
+                { "month", 2 }
+            };
+
+            public static void ApplyUpMigrations(MigrationBuilder migrationBuilder)
+            {
+                migrationBuilder.ConvertIntArrayToEnumArray(
+                    "representatives",
+                    "topics",
+                    EnumMappings.TopicEntityIntToEnum,
+                    "topic_entity"
+                );
+
+                migrationBuilder.ConvertIntToEnum(
+                    "representatives",
+                    "role",
+                    EnumMappings.RoleEntityIntToEnum,
+                    "role_entity"
+                );
+
+                migrationBuilder.ConvertIntArrayToEnumArray(
+                    "notifications",
+                    "topics",
+                    EnumMappings.TopicEntityIntToEnum,
+                    "topic_entity"
+                );
+
+                migrationBuilder.ConvertIntToEnum(
+                    "meters",
+                    "max_inactivity_period_duration",
+                    EnumMappings.DurationEntityIntToEnum,
+                    "duration_entity"
+                );
+
+                migrationBuilder.ConvertIntArrayToEnumArray(
+                    "lines",
+                    "phases",
+                    EnumMappings.PhaseEntityIntToEnum,
+                    "phase_entity"
+                );
+
+                migrationBuilder.ConvertIntToEnum(
+                    "events",
+                    "level",
+                    EnumMappings.LevelEntityIntToEnum,
+                    "level_entity"
+                );
+
+                migrationBuilder.ConvertIntArrayToEnumArray(
+                    "events",
+                    "categories",
+                    EnumMappings.CategoryEntityIntToEnum,
+                    "category_entity"
+                );
+
+                migrationBuilder.ConvertIntToEnum(
+                    "events",
+                    "audit",
+                    EnumMappings.AuditEntityIntToEnum,
+                    "audit_entity"
+                );
+
+                migrationBuilder.ConvertIntToEnum(
+                    "egauge_aggregates",
+                    "interval",
+                    EnumMappings.IntervalEntityIntToEnum,
+                    "interval_entity"
+                );
+            }
+
+            public static void ApplyDownMigrations(MigrationBuilder migrationBuilder)
+            {
+                migrationBuilder.ConvertEnumArrayToIntArray(
+                    "representatives",
+                    "topics",
+                    EnumMappings.TopicEntityEnumToInt
+                );
+
+                migrationBuilder.ConvertEnumToInt(
+                    "representatives",
+                    "role",
+                    EnumMappings.RoleEntityEnumToInt
+                );
+
+                migrationBuilder.ConvertEnumArrayToIntArray(
+                    "notifications",
+                    "topics",
+                    EnumMappings.TopicEntityEnumToInt
+                );
+
+                migrationBuilder.ConvertEnumToInt(
+                    "meters",
+                    "max_inactivity_period_duration",
+                    EnumMappings.DurationEntityEnumToInt
+                );
+
+                migrationBuilder.ConvertEnumArrayToIntArray(
+                    "lines",
+                    "phases",
+                    EnumMappings.PhaseEntityEnumToInt
+                );
+
+                migrationBuilder.ConvertEnumToInt(
+                    "events",
+                    "level",
+                    EnumMappings.LevelEntityEnumToInt
+                );
+
+                migrationBuilder.ConvertEnumArrayToIntArray(
+                    "events",
+                    "categories",
+                    EnumMappings.CategoryEntityEnumToInt
+                );
+
+                migrationBuilder.ConvertEnumToInt(
+                    "events",
+                    "audit",
+                    EnumMappings.AuditEntityEnumToInt
+                );
+
+                migrationBuilder.ConvertEnumToInt(
+                    "egauge_aggregates",
+                    "interval",
+                    EnumMappings.IntervalEntityEnumToInt
+                );
+            }
         }
     }
 }
