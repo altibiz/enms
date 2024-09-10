49138f651dd0b0ad455ae4a172a3d11126d9c9c8
Hrvoje Jurić <hrvoje@altibiz.com>
2024-09-10 10:28:57 +0200

    WIP

--- src/Enms.Business/Models/Complex/PeriodModel.cs
+++ src/Enms.Business/Models/Complex/PeriodModel.cs
@@ -1,7 +1,7 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text;
 using Enms.Business.Models.Abstractions;
 using Enms.Business.Models.Enums;
-using System.Text;
 
 namespace Enms.Business.Models.Complex;
 
